ec0a30b12721788da41b76ff9ab41b63f02ae9e5
Eno Jeon <gatojih0817@gmail.com>
2024-04-17 10:21:47 +0900

    Exit버튼 스크립트 작성(ButtonController에서 수정), 세팅(예시)씬 생성 및 Button적용, 메인메뉴 씬 버튼 인터랙션 수정

--- BIOHUNT/Assets/Scripts/UI/ButtonController.cs
+++ BIOHUNT/Assets/Scripts/UI/ButtonController.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class ButtonController : MonoBehaviour
 {
     public void NewGameButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+    
+    public void ExitGameButton()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void SettingButton()
     {
         SceneManager.LoadScene(1);
     }
